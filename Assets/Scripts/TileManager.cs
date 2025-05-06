06ddbfeb086828d1c62b96fa29efb83fc5035f9a
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2025-05-06 20:46:05 +0200

    added rts camera to testWorld Scene

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -7,7 +7,6 @@ using UnityEngine.Tilemaps;
 public class TileManager : MonoBehaviour
 {
     [SerializeField] private Tilemap map;
-    
     [SerializeField] private List<TileData> tileDatas;
     private Dictionary<TileBase, TileData> dataFromTiles;
 
@@ -28,26 +27,47 @@ public class TileManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int gridPos = map.WorldToCell(mousePos);
-            
-            
-            var clickedTileData = getTileDataByCoords(mousePos);
+            var mousePos = Input.mousePosition;
+            var worldPos = getWorldPositionOnPlane(mousePos);
 
+            
+            var clickedTileData = getTileDataByCoords(worldPos);
+
+            Vector3Int gridPos = map.WorldToCell(worldPos);
+            
+            if (clickedTileData == null)
+            {
+                print("At position " + gridPos + " there is no tile ");
+                return;
+            }
+            
             string tileType = clickedTileData.tileType;
             float travelCost = clickedTileData.travelCost;
             float waterValue = clickedTileData.waterValue;
             float landFertility = clickedTileData.landFertility;
             
-            print(clickedTileData != null ? "At position " + gridPos + " there is a " + tileType 
-                + " Tile with: \nTravelCost=" + travelCost + ", WaterValue=" + waterValue + ", LandFertility=" + landFertility 
-                                      : "At position " + gridPos + " there is no tile ");
+            print("At position " + gridPos + " there is a " + tileType 
+                + " Tile with: \nTravelCost=" + travelCost + ", WaterValue=" + waterValue + ", LandFertility=" + landFertility);
         }
     }
 
-    public TileData getTileDataByCoords(Vector2 coordinates)
+    public TileData getTileDataByCoords(Vector3 coordinates)
     {
-        Vector3Int gridPos = map.WorldToCell(coordinates);
-        return dataFromTiles[map.GetTile(gridPos)];
+        print(coordinates);
+        Vector3Int gridPos = map.WorldToCell(new Vector3(coordinates.x, coordinates.z, coordinates.y));
+        
+        print(gridPos);
+        
+        var tile = map.GetTile(gridPos);
+        return tile != null ? dataFromTiles[tile] : null;
+    }
+
+    
+    public Vector3 getWorldPositionOnPlane(Vector3 screenPosition) {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0.78f, 0));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
     }
 }
