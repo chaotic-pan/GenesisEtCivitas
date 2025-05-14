3c2f3ffca2a5591fde8bf193468733b0f0e534b9
FlyingBaum <kristgsus@gmail.com>
2025-05-14 16:02:45 +0200

    refactor(scripts): removed start and update

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -5,14 +5,4 @@ public class PlayerModel : MonoBehaviour
 
     public int influencePoints = 100;
     public int maxIP = 1000;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
-
 }
