bfd2d05fac950730c2ac14323348e9baabec3572
LiloTM <Laura-U@kabelmail.de>
2025-05-30 19:04:51 +0200

    edit size of the Genesis map

--- Assets/Scripts/MapGeneration/MapDisplay.cs
+++ Assets/Scripts/MapGeneration/MapDisplay.cs
@@ -8,11 +8,14 @@ namespace Terrain
     public class MapDisplay : MonoBehaviour
     {
         public GameObject meshRendererPrefab;
+        private const int size = 239;
+        // Scale needs to be the same size as the MapChunk prefab
+        private int scale = 1;
         public void DrawMeshes(Dictionary<Vector2,MeshData> meshDataDict, Dictionary<Vector2,Texture2D> textures)
         {
             foreach (var chunkCoordinate in meshDataDict.Keys)
             {
-                var newMesh = Instantiate(meshRendererPrefab, new Vector3(2390 * chunkCoordinate.x, 0, -2390 * chunkCoordinate.y), Quaternion.identity);
+                var newMesh = Instantiate(meshRendererPrefab, new Vector3(scale * size * chunkCoordinate.x, 0, -scale * size * chunkCoordinate.y), Quaternion.identity);
                 
                 var meshData = meshDataDict[chunkCoordinate];
                 newMesh.GetComponent<MeshFilter>().sharedMesh = meshData.CreateMesh();
