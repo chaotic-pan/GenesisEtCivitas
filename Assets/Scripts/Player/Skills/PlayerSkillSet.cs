8fa351889c44938030dd1f66e4ce15cfb5d894a6
Ni-Ru <80821728+Ni-Ru@users.noreply.github.com>
2025-06-11 01:52:55 +0200

    Skill Tree Functionality

--- Assets/Scripts/Player/Skills/PlayerSkillSet.cs
+++ Assets/Scripts/Player/Skills/PlayerSkillSet.cs
@@ -1,33 +1,83 @@
+using System;
 using System.Collections.Generic;
 
 namespace Player.Skills
 {
     public class PlayerSkillSet
     {
+
+        public event EventHandler<OnSkillUnlockedEventArgs> OnSkillUnlocked;
+        public class OnSkillUnlockedEventArgs : EventArgs
+        {
+            public Skill skill;
+        }
         public enum Skill
         {
+            None,
             WaterOne,
             WaterTwo,
             DeathOne,
             DeathTwo
         }
-        
+
         public PlayerSkillSet()
         {
             _unlockedSkills = new List<Skill>();
         }
-        
+
 
         private List<Skill> _unlockedSkills;
 
-        public void UnlockSkill(Skill skill)
+        private void UnlockSkill(Skill skill)
         {
-            _unlockedSkills.Add(skill);
+            if (!IsSkillUnlocked(skill))
+            {
+                _unlockedSkills.Add(skill);
+                OnSkillUnlocked?.Invoke(this, new OnSkillUnlockedEventArgs { skill = skill });
+            }
         }
 
         public bool IsSkillUnlocked(Skill skill)
         {
             return _unlockedSkills.Contains(skill);
+        }
+
+        public override string ToString()
+        {
+            return $"Unlocked Skills: {string.Join(", ", _unlockedSkills)}";
+        }
+
+        public Skill GetSkillRequirement(Skill skill)
+        {
+            switch (skill)
+            {
+                case Skill.WaterTwo: return Skill.WaterOne;
+                case Skill.DeathTwo: return Skill.DeathOne;
+            }
+            return Skill.None;
+        }
+
+        public bool TryUnlockSkill(Skill skill)
+        {
+            Skill skillRequirement = GetSkillRequirement(skill);
+
+            if (skillRequirement != Skill.None)
+            {
+                if (IsSkillUnlocked(skillRequirement))
+                {
+                    UnlockSkill(skill);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                UnlockSkill(skill);
+                return true;
+            }
         }
     }
 }
