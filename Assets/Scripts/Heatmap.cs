627ed9d6a174d410588bd35c0dd2ee642d358d6b
vap <kivuli@web.de>
2025-06-13 14:24:11 +0200

    add heatmaps

---
+++ Assets/Scripts/Heatmap.cs
@@ -0,0 +1,11 @@
+﻿using Terrain;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Heatmap", menuName = "ScriptableObjects/Heatmap")]
+public class Heatmap : ScriptableObject
+{
+    public MapDisplay.MapOverlay overlay;
+    public Gradient gradient;
+    public int min;
+    public int max;
+}
---
+++ Assets/Scripts/UI/UIHeatmaps.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Terrain;
+using TMPro;
+using UI;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Dropdown))]
+public class UIHeatmaps : MonoBehaviour
+{
+    private TMP_Dropdown _dropdown;
+
+    private void Awake()
+    {
+        _dropdown = GetComponent<TMP_Dropdown>();
+    }
+
+    private void Start()
+    {
+        foreach (var overlay in Enum.GetValues(typeof(MapDisplay.MapOverlay)))
+        {
+            _dropdown.AddOptions(new List<string>() { overlay.ToString() });
+        }
+    }
+    
+    public void OnSelectedMap(int choice)
+    {
+        var overlay = (MapDisplay.MapOverlay) choice;
+        UIEvents.UIMap.OnOpenTest.Invoke(overlay);
+    }
+}
