2ba4c0241c3aa75b31e710d04fae76024b7eb98a
FlyingBaum <kristgsus@gmail.com>
2025-06-25 07:20:28 +0200

    feat(rain-ability): implement AoE water increase and heatmap updates for affected tiles

--- Assets/Scripts/Player/Abilities/RainAbility.cs
+++ Assets/Scripts/Player/Abilities/RainAbility.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Terrain;
+using UI;
 
 namespace Player.Abilities
 {
@@ -13,11 +16,41 @@ namespace Player.Abilities
             Debug.Log("Entered Rain Ability");
         }
 
-        public override void CastAbility(Vector3Int tileGridPos)
+        public override void CastAbility(Vector3Int centerTilePos)
         {
-            // Debug.Log("Casted Rain Ability");
-            var tileData = TileManager.Instance.getTileDataByGridCoords(tileGridPos);
-            tileData.waterValue += 20;
+            // Get all tiles in AoE.
+            int radius = (EffectDiameter - 1) / 2;
+            List<Vector3Int> affectedTiles = TileManager.Instance.GetSpecificRange(centerTilePos, radius);
+            
+            // Apply water increase to each tile.
+            foreach (Vector3Int tilePos in affectedTiles)
+            {
+                var tileData = TileManager.Instance.getTileDataByGridCoords(tilePos);
+                if (tileData != null)
+                {
+                    tileData.waterValue += 20;
+                }
+            }
+            
+            // Update heatmaps for all affected chunks.
+            HashSet<Vector2> affectedChunks = new HashSet<Vector2>();
+            foreach (Vector3Int tilePos in affectedTiles)
+            {
+                var chunks = TileManager.Instance.getWorldPositionOfTile(tilePos);
+                foreach (Vector2 chunk in chunks)
+                {
+                    affectedChunks.Add(chunk);
+                }
+            }
+            
+            // Trigger heatmap updates.
+            foreach (Vector2 chunk in affectedChunks)
+            {
+                UIEvents.UIMap.OnUpdateHeatmapChunks.Invoke(
+                    new List<Vector2> { chunk }, 
+                    MapDisplay.MapOverlay.WaterValue
+                );
+            }
         }
     }
 }
